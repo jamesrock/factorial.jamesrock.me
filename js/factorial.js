552d5c2a6d7612720ef97b880f618e05f44ba481
James Rock <hello@jamesrock.me>
2017-02-20 20:25:31 +0000

    small rework

--- js/factorial.js
+++ js/factorial.js
@@ -1,63 +1,59 @@
 (function() {
 	
 	var
+	types = {
+		"long": function(n) {
+
+			n = n.toString();
+			
+			var 
+			_split = n.split("e+"),
+			first = _split[0].replace(".", ""),
+			second = _split[1],
+			zeroes = Number(second);
+
+			zeroes = (zeroes - (first.length-1));
+
+			while(zeroes--) {
+				first += "0";
+			};
+
+			return first;
+
+		},
+		"formatted": function(n) {
+
+			n = n.toString();
+
+			var
+			length = n.length,
+			_split = n.split("").reverse();
+
+			while(length--) {
+
+				if(length%3===0&&length>0) {
+					_split.splice(length, 0, ",");
+				};
+
+			};
+
+			return _split.reverse().join("");
+
+		},
+		"number": function(n) {
+			return n;
+		}
+	},	
 	factorial = function(i, type) {
 
 		var 
-		o = i,
-		outs = {
-			"N": 0,
-			"L": 0,
-			"F": 0
-		};
+		o = i;
 
 		while(i>1&&i--) {
 			o = (o*i);
 		};
 
-		outs["N"] = o;
-		outs["L"] = toLong(outs["N"]);
-		outs["F"] = toFormatted(outs["L"]);
-
-		return outs[type];
-
-	},
-	toLong = function(n) {
-
-		n = n.toString();
-		
-		var 
-		_split = n.split("e+"),
-		first = _split[0].replace(".", ""),
-		second = _split[1],
-		zeroes = Number(second);
-
-		zeroes = (zeroes - (first.length-1));
-
-		while(zeroes--) {
-			first += "0";
-		};
-
-		return first;
-
-	},
-	toFormatted = function(n) {
-
-		n = n.toString();
-
-		var
-		length = n.length,
-		_split = n.split("").reverse();
-
-		while(length--) {
-
-			if(length%3===0&&length>0) {
-				_split.splice(length, 0, ",");
-			};
-
-		};
-
-		return _split.reverse().join("");
+		return types[type](o);
 
 	};
 
